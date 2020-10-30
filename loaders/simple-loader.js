30eded2d9e84c41214905f582c7069d48d1be612
steam <jizhengqistrongman@126.com>
2020-10-30 12:11:01 +0800

    this.addDependency test; the former one is not right

--- loaders/simple-loader.js
+++ loaders/simple-loader.js
@@ -1,11 +1,8 @@
 const path = require('path');
 
 module.exports = function simpleLoader(content) {
-  const headerPath = path.resolve('../src/headerTxt.js');
+  const headerPath = path.resolve('./src/headerTxt2.js');
   this.addDependency(headerPath);
-  if(this.resourcePath.endsWith('headerTxt.js')) {
-    this.emitWarning(new Error('WARNING: headerTxt!!!!!'));
-  }
 
   return content.replace(/world/i, 'simple-loader');
 }
