e94fd9fa3c974ec7981359eb418ea688ea55e3a9
steam <jizhengqistrongman@126.com>
2020-10-30 11:55:57 +0800

    add webpack-dev-server and test this.addDependency

--- loaders/simple-loader.js
+++ loaders/simple-loader.js
@@ -1,3 +1,8 @@
+const path = require('path');
+
 module.exports = function simpleLoader(content) {
+  const headerPath = path.resolve('../src/headerTxt.js');
+  this.addDependency(headerPath);
+
   return content.replace(/world/i, 'simple-loader');
 }
