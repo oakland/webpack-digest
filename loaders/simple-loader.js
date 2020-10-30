607e0a734d04338c8f89bac47ca47399a339dfee
steam <jizhengqistrongman@126.com>
2020-10-30 12:20:11 +0800

    add loader-utils and test this.emitFile

--- loaders/simple-loader.js
+++ loaders/simple-loader.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const loaderUtils = require("loader-utils");
 
 module.exports = function simpleLoader(content) {
-  const headerPath = path.resolve('./src/headerTxt2.js');
-  this.addDependency(headerPath);
+  const outputPath = loaderUtils.interpolateName(this, 'test.txt', {});
+  this.emitFile(outputPath, 'test');
 
   return content.replace(/world/i, 'simple-loader');
 }
