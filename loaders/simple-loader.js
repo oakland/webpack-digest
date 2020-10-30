de1868e253f7da9c45f96d5290d6af605fdb8e4d
steam <jizhengqistrongman@126.com>
2020-10-30 12:03:30 +0800

    test this.resourcePath and this.emitWarning

--- loaders/simple-loader.js
+++ loaders/simple-loader.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = function simpleLoader(content) {
   const headerPath = path.resolve('../src/headerTxt.js');
   this.addDependency(headerPath);
+  if(this.resourcePath.endsWith('headerTxt.js')) {
+    this.emitWarning(new Error('WARNING: headerTxt!!!!!'));
+  }
 
   return content.replace(/world/i, 'simple-loader');
 }
