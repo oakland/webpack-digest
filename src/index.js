5814b9505fdc42d4f376f438274c309b3020c29b
steam <jizhengqistrongman@126.com>
2020-11-02 22:59:03 +0800

    plugin patterns

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-//import headerTxt from './headerTxt.js';
-import headerTxt from "!!../loaders/loader3.js!./headerTxt.js";
-import box from './img/box.png';
-import styles from './styles.scss';
+import headerTxt from "./headerTxt.js";
 
 window.onload = function() {
-  // 1. create elements
   const body = document.querySelector('body');
   const container = document.createElement('div');
   const header = document.createElement('h3');
-  const img = document.createElement('img');
 
-  // 2. assign class to elements
-  container.className = 'container';
-  header.className = 'header';
-  img.className = 'img';
-
-  // 3. put elements into body
   header.innerText = headerTxt;
-  img.src = box;
   container.appendChild(header);
-  container.appendChild(img);
   body.appendChild(container);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MyPlugin = require('./plugins/my-plugin');
 
 exports.default = {
   mode: 'development', // 为了方便查看打包后的内容，我们采用 development mode
@@ -8,39 +9,8 @@ exports.default = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          path.join(__dirname, '/loaders/loader1'),
-          path.join(__dirname, '/loaders/loader2'),
-          path.join(__dirname, '/loaders/loader3')
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 1500, // 改成 2000 后会被编码成 base64
-              outputPath: 'img'
-            }
-          }
-        ]
-      }
-    ]
-  },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
+    new MyPlugin()
   ]
 }
