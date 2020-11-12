9f18385328ccbd330e47a655372d63a848b5bf75
steam <jizhengqistrongman@126.com>
2020-11-12 08:37:32 +0800

    moduleConcatenation test(scope hoisting)

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ const MyPlugin = require('./plugins/my-plugin');
 exports.default = {
   mode: 'development', // 为了方便查看打包后的内容，我们采用 development mode
   entry: './src/index.js',
+  optimization: {
+    concatenateModules: true // this will concatenate index.js and headerText.js to one module
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
